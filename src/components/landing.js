cc1db622309df96136a894a43363002ae58e7092
Saad Khalid <saadkhalid90@gmail.com>
2022-07-13 17:10:48 +0500

    start

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,18 +1,24 @@
 import video75 from "../resources/landing/video75.mp4";
+import lbfLogo from "../resources/LBF - Logo.png";
 import styles from "./css-modules/landing.module.css";
 
 function Landing() {
   return (
     <div className={styles.landingContain}>
-      <h3 className={styles.titleLBF}>LBF VIRTUAL MUSEUM</h3>
-      <div className={styles.videoContain}>
-        <video width="550" autoPlay loop muted>
-          <source src={video75} type="video/mp4" />
-        </video>
+      <div className={styles.landingContent}>
+        <div className={styles.logoContain}>
+          <img src={lbfLogo}></img>
+        </div>
+        <h3 className={styles.titleLBF}>LBF VIRTUAL MUSEUM</h3>
+        <div className={styles.videoContain}>
+          <video width="350" autoPlay loop muted>
+            <source src={video75} type="video/mp4" />
+          </video>
+        </div>
+        <p className={styles.titleDesc}>
+          Years of Cultural Histories of Pakistan
+        </p>
       </div>
-      <p className={styles.titleDesc}>
-        Years of Cultural Histories of Pakistan
-      </p>
     </div>
   );
 }
